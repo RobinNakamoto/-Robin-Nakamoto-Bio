0788723fc02ea02ebd5317bb746579ad2009d1af
Robin Nakamoto <143897501+RobinNakamoto@users.noreply.github.com>
2023-09-09 01:58:19 +0100

    Create bio.py

---
+++ bio.py
@@ -0,0 +1,23 @@
+username = 'RobinNakamoto'
+profile_image = 'https://avatars.githubusercontent.com/u/143897501?v=4'
+bio = {
+    'persona': 'rule_bender',
+    'focus': ['hacking', 'pen_testing', 'system_exploitation'],
+    'skills': ['OSINT', 'day_trading'],
+    'learning_focus': 'Python',
+    'philosophy': 'Challenge norms, dissect systems',
+    'worldview': 'anarchic',
+    'dislikes': 'banks',
+    'companions': ['ChatGPT'],
+    'traits': ['nerd', 'curious'],
+    'social_media': ['X'],
+    'attributes': {
+        'music_preference': 'Techno',
+        'political_view': 'Anarchic',
+        'attitude': 'Exploitative',
+    },
+    'commitments': ('1/2 Hacker', '1/2 Bitcoin Trader'),
+    'motto': 'Fcuk the system'
+    'profile_image': 'https://avatars.githubusercontent.com/u/143897501?v=4'
+
+}
